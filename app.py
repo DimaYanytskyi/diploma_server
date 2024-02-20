579652e094cb09981499a423d6b2c3e85484ca88
romancukoleksandr40 <romancukoleksandr40@gmail.com>
2024-02-20 23:51:09 +0200

    changed imports

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 from flask import Flask, request, jsonify
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
 from google.cloud import firestore
-from google.cloud.firestore import SERVER_TIMESTAMP
 import os
 import json
 
 service_account_info = json.loads(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
-
 cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
-
-db = firestore.Client()
 
 app = Flask(__name__)
 
 
 @app.route('/post-data', methods=['POST'])
 def post_data():
-    data = request.json
-    collection_id = data.get('mac', 'defaultCollection')
-    document_id = data.get('documentId', 'defaultDocument')
+    try:
+        data = request.json
+        collection_id = data.get('mac', 'defaultCollection')
+        document_id = data.get('documentId', 'defaultDocument')
 
-    doc_ref = db.collection(collection_id).document(document_id)
-    doc_ref.set({
-        'timestamp': SERVER_TIMESTAMP,
-        'data': data
-    })
+        doc_ref = firestore.client().collection(collection_id).document(document_id)
 
-    return jsonify({"status": "success", "message": "Data posted to Firestore successfully."}), 200
+        doc_ref.set({
+            'timestamp': firestore.SERVER_TIMESTAMP,
+            'data': data
+        })
+
+        return jsonify({"status": "success", "message": "Data posted to Firestore successfully."}), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
